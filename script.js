f1472319f2c72bc9628799f969f5f38681bfd594
ozkanbuyuk <ozkanbuyuk@icloud.com>
2023-12-27 17:22:39 +0300

    JavaScript To-Do App

---
+++ script.js
@@ -0,0 +1,25 @@
+const input = document.querySelector("#add");
+const btn = document.querySelector("#btn");
+const list = document.querySelector("#list");
+
+btn.addEventListener("click", btnClick);
+list.addEventListener("click", listClick);
+
+function btnClick() {
+  var txt = input.value;
+  if (txt == "") {
+    alert("You must write something!");
+  } else {
+    li = document.createElement("li");
+    li.classList.add("element-group__list__item");
+    li.innerHTML = txt;
+    list.insertBefore(li, list.childNodes[0]);
+    input.value = "";
+  }
+}
+
+function listClick(e) {
+  if (e.target.tagName == "LI") {
+    e.target.classList.toggle("checked");
+  }
+}
